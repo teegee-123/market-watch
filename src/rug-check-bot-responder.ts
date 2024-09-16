253546c6c9fc884693fd2b44217b9f7857df2fcf
teegee-123 <u10132806@gmail.com>
2024-09-16 23:01:29 +0200

    Update rug-check-bot-responder.ts

--- src/rug-check-bot-responder.ts
+++ src/rug-check-bot-responder.ts
@@ -20,7 +20,7 @@ export async function startRugListener(rugcheckbot: TelegramBot) {
 
     rugcheckbot.on('message', async (msg, meta) => {
         
-        const addresses = msg.text?.match("[A-Za-z0-9]{44}") 
+        const addresses = JSON.stringify(msg).match("[A-Za-z0-9]{44}") 
         console.log(msg)
         console.log(msg.text?.match("[A-Za-z0-9]{44}"))
         if(addresses?.length){
